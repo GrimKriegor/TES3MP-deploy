b9ecc86d256e69edd63fffa20bc7938ce05faa95
Grim Kriegor <grimkriegor@krutt.org>
2017-08-12 00:21:45 +0100

    Tweaks to package name and cleanup process

--- tes3mp-deploy.sh
+++ tes3mp-deploy.sh
@@ -543,9 +543,9 @@ if [ $MAKE_PACKAGE ]; then
   echo -e "\n>> Creating TES3MP package"
 
   PACKAGE_BINARIES=("tes3mp" "tes3mp-browser" "tes3mp-server" "openmw-launcher" "openmw-wizard" "openmw-essimporter" "openmw-iniimporter" "bsatool" "esmtool")
-  LIBRARIES_OPENMW=("libavcodec.so" "libavformat.so" "libavutil.so" "libboost_filesystem.so" "libboost_program_options.so" "libboost_system.so" "libboost_thread.so" "libBulletCollision.so" "libbz2.so" "libfreetype.so" "libLinearMath.so" "libMyGUIEngine.so" "libopenal.so" "libOpenThreads.so" "libosgAnimation.so" "libosgDB.so" "libosgFX.so" "libosgGA.so" "libosgParticle.so" "libosg.so" "libosgText.so" "libosgUtil.so" "libosgViewer.so" "libosgWidget.so" "libSDL2" "libswresample.so" "libswscale.so" "libts.so" "libtxc_dxtn.so" "libunshield.so" "libuuid.so" "osgPlugins")
+  LIBRARIES_OPENMW=("libavcodec.so" "libavformat.so" "libavutil.so" "libboost_filesystem.so" "libboost_program_options.so" "libboost_system.so" "libboost_thread.so" "libBulletCollision.so" "libbz2.so" "libLinearMath.so" "libMyGUIEngine.so" "libopenal.so" "libOpenThreads.so" "libosgAnimation.so" "libosgDB.so" "libosgFX.so" "libosgGA.so" "libosgParticle.so" "libosg.so" "libosgText.so" "libosgUtil.so" "libosgViewer.so" "libosgWidget.so" "libSDL2" "libswresample.so" "libswscale.so" "libts.so" "libtxc_dxtn.so" "libunshield.so" "libuuid.so" "osgPlugins") #"libfreetype.so"
   LIBRARIES_TES3MP=("libcallff.a" "libRakNetLibStatic.a" "libterra.a")
-  LIBRARIES_EXTRA=("libx265.so" "libwebpmux.so" "libwebp.so" "libvpx.so" "libtwolame.so" "libshine.so" "libopenjpeg.so" "libcrystalhd.so" "libssh-gcrypt.so" "libbluray.so" "libchromaprint.so" "libpng16.so" "libpng12.so" "libsndio.so" "libopenmpt.so")
+  LIBRARIES_EXTRA=("libpng16.so" "libpng12.so") #"libstdc++.so.6"
 
   #EXIT IF TES3MP hasn't been compiled yet
   if [ ! -f "$DEVELOPMENT"/tes3mp ]; then
@@ -559,6 +559,7 @@ if [ $MAKE_PACKAGE ]; then
   #CLEANUP UNNEEDED FILES
   echo -e "\nCleaning up unneeded files"
   find "$PACKAGE_TMP" -type d -name "CMakeFiles" -exec rm -rf "{}" \; || true
+  #find "$PACKAGE_TMP" -type d -name ".git" -exec rm -rf "{}" \; || true
   find "$PACKAGE_TMP" -type l -exec rm -f "{}" \; || true
   rm -f ./*.bkp
 
@@ -613,7 +614,7 @@ if [ $MAKE_PACKAGE ]; then
   PACKAGE_DISTRO=$(lsb_release -si)
   PACKAGE_VERSION=$(cat "$CODE"/components/openmw-mp/Version.hpp | grep TES3MP_VERSION | awk -F'"' '{print $2}')
   PACKAGE_COMMIT=$(git --git-dir=$CODE/.git rev-parse @ | head -c10)
-  PACKAGE_NAME="tes3mp-$PACKAGE_SYSTEM-$PACKAGE_ARCH-$PACKAGE_DISTRO-release-$PACKAGE_VERSION-$PACKAGE_COMMIT-$USER"
+  PACKAGE_NAME="tes3mp-$PACKAGE_SYSTEM-$PACKAGE_ARCH-release-$PACKAGE_VERSION-$PACKAGE_COMMIT-$PACKAGE_DISTRO-$USER"
   PACKAGE_DATE="$(date +"%Y-%m-%d")"
   echo -e "TES3MP $PACKAGE_VERSION ($PACKAGE_COMMIT) built on $PACKAGE_SYSTEM $PACKAGE_ARCH ($PACKAGE_DISTRO) on $PACKAGE_DATE by $USER" > "$PACKAGE_TMP"/tes3mp-package-info.txt
 

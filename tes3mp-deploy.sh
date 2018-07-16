4949381b466211a1081378cc436959e4ac3179b4
Grim Kriegor <grimkriegor@krutt.org>
2018-07-16 01:08:22 +0100

    Remove RakNet compatibility hack

--- tes3mp-deploy.sh
+++ tes3mp-deploy.sh
@@ -595,12 +595,6 @@ Proceed at your own risk."
   mkdir -p "$DEPENDENCIES"/raknet/build
   cd "$DEPENDENCIES"/raknet/build
 
-  # Compatibility hack for 0.6.2, please remove once stable becomes 0.6.3
-  if [[ "$TARGET_COMMIT" == "0.6.2" || "$TARGET_COMMIT" == "stable" || "$TARGET_COMMIT" == "" ]]; then
-    git fetch --unshallow | true
-    git checkout 1d6bb9e88db04aaeaa8752835c17574509d05a31
-  fi
-
   rm -f CMakeCache.txt
   cmake -DCMAKE_BUILD_TYPE=Release -DRAKNET_ENABLE_DLL=OFF -DRAKNET_ENABLE_SAMPLES=OFF -DRAKNET_ENABLE_STATIC=ON -DRAKNET_GENERATE_INCLUDE_ONLY_DIR=ON ..
   make -j$CORES

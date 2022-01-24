bb449b0949fe8e69f4ec4082ffebbea0c36c2a7d
Grim Kriegor <grimkriegor@krutt.org>
2022-01-24 00:22:30 +0000

    Upgrade CrabNet revision

--- tes3mp-deploy.sh
+++ tes3mp-deploy.sh
@@ -582,7 +582,7 @@ Proceed at your own risk."
   # Build RakNet
   echo -e "\n>> Building RakNet"
   cd "$DEPENDENCIES"/raknet
-  git checkout 4eeeaad2f6c11aeb82070df35169694b4fb7b04b
+  git checkout 19e66190e83f53bcdcbcd6513238ed2e54878a21
 
   mkdir -p "$DEPENDENCIES"/raknet/build
   cd "$DEPENDENCIES"/raknet/build
